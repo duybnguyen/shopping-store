d05d2323cdd340f0363092f34f12403a1fd05ec2
Duy-B-Nguyen <duybnguyen04@gmail.com>
2024-07-10 14:45:32 -0400

    navbar search now links to the product

--- src/routes/Navbar/Navbar.jsx
+++ src/routes/Navbar/Navbar.jsx
@@ -1,11 +1,50 @@
 import "./Navbar.scss";
-import searchIcon from '../../assets/search-icon.png'
-import cartIcon from '../../assets/cart-icon.png'
+import searchIcon from "../../assets/search-icon.png";
+import cartIcon from "../../assets/cart-icon.png";
 import { Link } from "react-router-dom";
-const Navbar = () => {
-  
-  return (
+import { useEffect, useState } from "react";
 
+const Navbar = () => {
+  const [itemsData, setItemsData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [isFocused, setIsFocused] = useState(false);
+
+  useEffect(() => {
+    const getStoreItems = async () => {
+      try {
+        const items = await fetch(`https://fakestoreapi.com/products`, {
+          mode: "cors",
+        });
+        const response = await items.json();
+        setItemsData(response);
+        setFilteredData(response);
+      } catch (error) {
+        console.log(`Error while fetching data: ${error}`);
+      }
+    };
+
+    getStoreItems();
+  }, []);
+
+  const handleSearch = (e) => {
+    const query = e.target.value;
+    setSearch(query);
+    const filtered = itemsData
+      .filter((item) => item.title.toLowerCase().includes(query.toLowerCase()))
+      .slice(0, 5); // Limit to top 5 results
+    setFilteredData(filtered);
+  };
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setTimeout(() => setIsFocused(false), 200);
+  };
+
+  return (
     <div className="navbar-container">
       <Link to="/" className="logo">
         <h2 className="left">Fashion</h2>
@@ -13,20 +52,45 @@ const Navbar = () => {
       </Link>
 
       <div className="search-container">
-        <img src={searchIcon} alt="search icon" />
-        <input type="text" className="search" placeholder="Search store"/>
+        <div className="search-bar">
+          <img src={searchIcon} alt="search icon" />
+          <input
+            type="text"
+            className="search"
+            placeholder="Search store"
+            onChange={handleSearch}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          />
+        </div>
+        {search && isFocused && (
+          <div className="filtered-search-container">
+            {filteredData.map((item) => (
+              <div className="item-container" key={item.id}>
+                <Link
+                  className="link"
+                  to={`/products/${item.id}`}
+                  state={{ item }}
+                >
+                  <img src={item.image} alt="item image" />
+                  <div className="item-description">
+                    <h2>{item.title}</h2>
+                    <p>${item.price}</p>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
 
       <div className="words-container">
         <Link to="/products">Products</Link>
-
         <Link to="/about">About</Link>
-
-        <img src={cartIcon} alt="cart icon" className="cart"/>
+        <img src={cartIcon} alt="cart icon" className="cart" />
       </div>
-
     </div>
-  )
+  );
 };
 
-export default Navbar
+export default Navbar;
